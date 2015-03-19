9f780930fad7a0474890f96fbebb33075e82a5f3
Dolly3g <dollygyanchandani1996@gmail.com>
2015-03-19 18:12:37 +0530

    statement gets a customer now.f

--- src/com/twu/refactor/Customer.java
+++ src/com/twu/refactor/Customer.java
@@ -13,31 +13,40 @@ public class Customer {
 		rentalList.add(arg);
 	}
 
-    private Statement calculateRentalSubTotal() {
+    public Map<String, Object> calculateRentalSubTotal() {
+        Map<String,Object> summary = new HashMap<String, Object>();
         Map<String, Double> rentalSubTotal = new LinkedHashMap<String,Double>();
         double totalAmount = 0;
+        System.out.println(rentalList);
         for(Rental rental : rentalList){
             double amount = rental.getAmountFor();
             rentalSubTotal.put(rental.getMovie().getTitle(), amount);
             totalAmount += amount;
         }
         int frequentRenterPoints = calculateFrequentRenterPoints(rentalList);
-        return new Statement(name, rentalSubTotal,totalAmount,frequentRenterPoints);
+        summary.put("rentalSubTotal",rentalSubTotal);
+        summary.put("amount",totalAmount);
+        summary.put("frequentRenterPoints",frequentRenterPoints);
+        return summary;
     }
 
     private int calculateFrequentRenterPoints(List<Rental> rentalList) {
         int frequentRenterPoints = 0;
-        for(Rental rental : rentalList){
-            frequentRenterPoints = rental.isNewRelease() ? frequentRenterPoints+2 : frequentRenterPoints+1;
-   }
+        for(Rental rental : rentalList) {
+            frequentRenterPoints = rental.isNewRelease() ? frequentRenterPoints + 2 : frequentRenterPoints + 1;
+        }
         return frequentRenterPoints;
     }
 
     public String getStatement() {
-        return calculateRentalSubTotal().toString();
+        return new Statement(this).toString();
 	}
 
     public String htmlStatement() {
-        return calculateRentalSubTotal().toHTML();
+        return new Statement(this).toHTML();
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/com/twu/refactor/Statement.java
+++ src/com/twu/refactor/Statement.java
@@ -9,43 +9,40 @@ import java.util.Map;
  * Created by dollyg on 3/17/2015.
  */
 public class Statement {
-    private String customerName;
-    private final Map<String, Double> rentalSubTotal;
-    private double amount;
-    private int frequentRenterPoints;
+    Customer customer;
 
-    public Statement(String customerName, Map<String, Double> rentalSubTotal, double amount, int frequentRenterPoints) {
-
-        this.customerName = customerName;
-        this.rentalSubTotal = rentalSubTotal;
-        this.amount = amount;
-        this.frequentRenterPoints = frequentRenterPoints;
+    public Statement(Customer customer) {
+        this.customer = customer;
     }
 
 
     @Override
     public String toString() {
+        Map<String, Object> summary = customer.calculateRentalSubTotal();
+        Map<String,Double> rentalSubTotal = (Map<String,Double>)summary.get("rentalSubTotal");
         String textStatement = "Rental Record for #{customerName}\n#{rentalSummary}" +
                 "Amount owed is #{amount}\nYou earned #{frequentRenterPoints} frequent renter" +
                 " points";
         String rentalSummaryTemplate = "\t#{movie}\t#{amount}\n";
         String rentalSummary = "";
-        textStatement = textStatement.replace("#{customerName}",customerName);
+        textStatement = textStatement.replace("#{customerName}",customer.getName());
         for (String movie : rentalSubTotal.keySet()){
             rentalSummary += rentalSummaryTemplate.replace("#{movie}",movie).replace("#{amount}",rentalSubTotal.get(movie)+"");
         }
         textStatement = textStatement.replace("#{rentalSummary}",rentalSummary);
-        textStatement = textStatement.replace("#{amount}",amount+"").replace("#{frequentRenterPoints}",frequentRenterPoints+"");
+        textStatement = textStatement.replace("#{amount}",summary.get("amount")+"").replace("#{frequentRenterPoints}",summary.get("frequentRenterPoints")+"");
         return textStatement;
     }
 
     public String toHTML() {
-        String statement = "<H1>Rentals for <EM>" + customerName + "</EM></H1><P>";
+        Map<String, Object> summary = customer.calculateRentalSubTotal();
+        Map<String,Double> rentalSubTotal = (Map<String,Double>)summary.get("rentalSubTotal");
+        String statement = "<H1>Rentals for <EM>" + customer.getName() + "</EM></H1><P>";
         for (String movie : rentalSubTotal.keySet()){
             statement += movie + ": " + rentalSubTotal.get(movie) + "<BR>";
         }
-        statement += "<P>" + "You owe <EM>" + amount + "</EM>" +
-            "<P>On this rental you earned <EM>" + frequentRenterPoints +
+        statement += "<P>" + "You owe <EM>" + summary.get("amount") + "</EM>" +
+            "<P>On this rental you earned <EM>" + summary.get("frequentRenterPoints") +
             "</EM> frequent renter points<P>";
         return statement;
     }
